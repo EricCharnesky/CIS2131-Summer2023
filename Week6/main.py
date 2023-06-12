4dc3172f42c81bc29056ed542c2a5b1a128ceb8b
Eric Charnesky <eric.charnesky@careevolution.com>
2023-06-12 01:57:00 -0400

    week 6

---
+++ Week6/main.py
@@ -0,0 +1,145 @@
+import random
+
+word_list = ["able","about","again","air","all","always","and","answer","any","around","ask","at","back","be","before","best","better","between","black","body","book","boy","brother","but","call","can","car","careful","cat","chair","chance","cheese","child","cinema","clean","clear","close","cold","come","could","country","cry","cut","dance","daughter","day","dinner","do","doctor","document","dog","door","down","dream","drink","each","easy","eat","egg","eight","end","everything","explain","eye","face","family","father","find","fire","first","for","friend","from","game","get","girl","give","good","hand","head","help","her","his","home","idea","important","information","inside","interesting","job","kind","know","land","learn","life","light","live","long","make","man","many","may","money","more","morning","move","my","name","new","no","now","often","one","open","or","out","page","paper","park","pay","peace","pen","people","person","picture","place","play","please","popular","prefer","problem","put","question","reach","read","ready","red","rest","rich","right","river","road","room","run","same","say","school","second","see","send","set","she","ship","shop","should","show","sit","small","so","some","son","soon","speak","stand","start","stone","stop","student","such","table","that","the","there","they","thing","think","this","time","today","two","understand","up","very","visit","wait","want","we","what","where","why","word","work","write","you	across","action","after","against","age","almost","alone","already","also","anything","army","art","away","bad","beautiful","because","become","bed","big","box","bread","breakfast","bring","bus","buy","camera","care","carry","catch","cause","certain","change","chief","church","city","class","company","confirm","continue","control","corner","cost","cover","culture","dead","dear","decision","deep","describe","desert","die","difficult","distance","dress","drive","dry","east","education","enjoy","enough","evening","every","example","expect","expensive","fact","fast","feel","few","fight","flower","food","free","full","garage","garden","gold","great","green","group","happy","hard","have","here","hope","house","how","ill","impossible","include","industry","interest","into","invite","island","journey","juice","keep","late","leave","let","letter","like","look","meet","member","miss","moment","month","most","mother","much","must","need","never","next","nothing","old","on","only","other","own","part","personal","phone","plan","player","police","position","possible","power","present","president","pretty","price","product","provide","public","quite","rather","real","reason","receive","record","remain","remember","report","result","return","round","sad","save","sea","seat","service","several","shall","side","single","sister","sleep","something","sorry","sound","south","spring","star","stay","story","street","strong","study","sun","sweet","system","take","talk","teacher","tell","their","then","through","together","too","true","try","under","use","view","voice","water","way","week","which","who","wife","with","woman","world","yes	above","accident","act","add","address","ago","airport","along","among","another","appear","apple","arm","baby","bear","begin","behind","believe","blood","blue","both","break","building","built","business","card","case","central","century","check","chicken","choose","clock","collect","colour","common","computer","condition","consider","course","court","crazy","cross","cup","dangerous","dark","decide","depend","desk","different","direct","direction","discuss","dollar","draw","during","ear","early","earth","edge","effect","even","ever","examine","exchange","fall","field","film","fine","fish","floor","fly","form","fruit","general","gift","glad","glass","government","grow","half","happen","hear","heart","high","hold","hour","ice","illness","imagine","immediately","increase","inform","insect","introduce","jump","just","key","less","line","little","love","low","magazine","mark","material","mean","meat","meeting","milk","mind","minute","nature","near","not","number","off","office","once","order","over","parent","party","pass","past","patient","pencil","perhaps","piece","pink","plate","poor","post","produce","protect","pull","purpose","quick","race","radio","rain","really","remove","reply","respect","ride","ring","rise","rock","search","secret","sell","sense","sharp","shoe","short","similar","simple","since","situation","slow","smile","snow","sometimes","song","speed","sport","state","station","still","success","sugar","summer","sure","tall","tea","television","test","than","though","ticket","town","travel","tree","turn","upon","usual","valley","value","virus","walk","war","watch","well","when","while","will","window","would","your","accept","account","actor","advance","advantage","afternoon","agree","amount","angry","arrange","article","attack","available","bag","bank","below","bird","boat","borrow","bottle","bridge","brown","build","busy","butter","cake","captain","centre","cheap","chocolate","choice","cigarette","cloth","cloud","club","coast","coat","coffee","college","comfortable","cottage","cream","crowd","customer","damage","date","deal","degree","demand","department","design","destroy","detail","dirty","dust","earn","enemy","engine","enter","escape","especially","everywhere","except","exercise","expression","factory","famous","farm","favourite","finger","finish","follow","front","future","gas","gate","gentle","gentleman","ground","guide","hair","hat","history","holiday","hospital","hotel","hungry","illegal","image","improve","independent","individual","injure","instrument","interview","joke","kitchen","knife","language","large","law","left","lesson","listen","machine","map","market","marry","meal","message","mistake","moon","music","newspaper","nice","night","nose","object","obtain","ocean","oil","orange","pain","paint","parcel","path","peaceful","perfect","petrol","plane","plant","pleasure","practise","prepare","prison","private","probably","proud","quiet","recently","refuse","relationship","religion","repair","repeat","replace","request","responsible","restaurant","rice","salt","science","season","seem","serious","serve","shape","shoulder","sick","silver","size","skirt","sky","smart","smoke","society","special","spend","square","step","store","strange","subject","successful","supply","swim","taste","teach","temperature","thank","tired","tomorrow","top","touch","train","trip","uncle","university","usually","vegetable","village","wall","warm","wash","weather","welcome","win","winter","wrong","yesterday","admire","admit","adventure","afraid","allow","although","animal","area","arrive","asleep","attempt","attend","attitude","average","bath","battle","beer","belong","bill","board","born","bottom","branch","brave","breathe","calm","careless","certainly","chain","character","charge","circle","climb","clothes","coal","coin","comfort","complete","consist","contain","cook","copy","correct","count","crime","death","declare","defend","delay","discover","dish","divide","double","doubt","drop","duty","effort","either","English","equal","event","exact","experience","express","fail","farmer","fat","flat","foot","foreign","forget","fresh","funny","goal","grey","guard","guess","guest","gun","hate","health","heavy","hide","hole","horse","hot","husband","indeed","influence","instead","intelligent","international","iron","jacket","join","kill","lake","last","laugh","leader","leg","lie","lose","main","marriage","match","measure","medicine","metal","middle","mine","modern","mountain","necessary","nervous","noise","offer","operation","opinion","opposite","outside","pair","passenger","perform","pile","pity","plain","pleasant","plenty","point","politics","pound","pour","press","prize","profit","promise","prove","raise","recent","reduce","regard","regular","risk","roll","rough","row","rule","rush","safe","score","secretary","series","share","shoot","shut","sign","sing","smell","smooth","soft","soldier","somewhere","space","speech","spoil","spot","spread","steal","straight","suddenly","suggest","suit","support","surface","these","thick","thin","third","those","toilet","trade","trouble","trust","type","until","useful","variety","various","vote","west","whose","wild","wish","without","wonderful","wood","year","young"]
+
+random_word = word_list[random.randint(0, len(word_list)-1)]
+
+print(random_word)
+
+#e rkrcnyihecas
+#'e r i c   c h a r n e  s  k  y'
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+# print_gallows(0)
+# _______
+# |    |
+# |
+# |
+# |
+# |
+# |
+# |
+# |
+# -----------
+
+# print_gallows(6)
+# _______
+# |    |
+# |    O
+# |  --|--
+# |    |
+# |   / \
+# |  /   \
+# |
+# |
+# -----------
+
+def get_gradebook_line(student_name, percentage_score):
+    return f"Name: {student_name} - Score: {percentage_score:.1f}"
+
+def vowel_count(word):
+    vowels = 0
+    for character in word.lower():
+        if character == "a" or character == "e" \
+                or character == "i" or character == "o" \
+                or character == "u":
+            vowels += 1
+    word = word.lower()
+    vowels = word.count("a") \
+        + word.count("e") \
+        + word.count("i") \
+        + word.count("o") \
+        + word.count("u")
+
+    return vowels
+
+
+print(get_gradebook_line("Eric", 89.95))
+
+# strings are IMMUTABLE - can't be changed in memory
+
+name = input("Enter your name")
+
+if " " in name:
+    # taking a slice with [start_index : end_index not including ]
+    index_of_space = name.find(" ")
+    first_name = name[0:index_of_space]
+    last_name = name[index_of_space+1:len(name)]
+    print("First:", first_name)
+    print("Last:", last_name)
+
+    print(first_name, "<", last_name, ":", first_name < last_name)
+    print(first_name, ">", last_name, ":", first_name > last_name)
+
+
+
+for character in name:
+    print(character)
+
+for index in range(len(name)):
+    # square braces meaning index of
+    print(index, ":", name[index])
+
+    # this crashses, you can't change characters inside a string
+    #name[index] = name[index].upper()
+
+print("Vowels in your name:", vowel_count(name))
+
+if "eric" in name.lower():
+    print("Hey, my name is Eric too!")
+
+print(name)
+
+print(name.lower())
+print(name.upper())
+
+name = name.lower() # makes a new string in memory
+# name points at the new location in memory
+
+name = name.replace("a", "x")
+name = name.replace("e", "x")
+name = name.replace("i", "x")
+name = name.replace("o", "x")
+name = name.replace("u", "x")
+
+print(name.title())
+
+
+
+email = ""
+
+# not good validation
+while (not (email.endswith(".com")
+        or email.endswith(".net")
+        or email.endswith(".edu"))) \
+        or "@" not in email:
+    # strip will remove leading and trailing white space
+    email = input("Enter your email").strip()
+    print("the at sign is in position", email.find('@'))
+
+index_of_at_sign = email.find("@")
+
+# slice defaults [ start_index = 0 : end_index = len(string) ]
+print("id", email[:index_of_at_sign])
+print("domain", email[index_of_at_sign+1:])
+
+print(email[:])
+
+# [ start_index : end_index : stride or step
+print(email[::2], email[1::2])
+print(email[::-1])
+
+office_number = int(input("Enter your office room #"))
+
+
+signature = "Name: " + name + "\n" \
+    + "Email: " + email + "\n" \
+    + "Office #" + str(office_number) + "\n" \
+    + "Professor of Computer Information Systems"
+
+signature = f'Name: {name}\n' \
+    + f'Email: {email}\n' \
+    + f'Office #{office_number}\n' \
+    + "Professor of Computer Information Systems"
+
+print(signature)
